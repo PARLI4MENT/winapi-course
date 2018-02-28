f6756653486b2b464366925afb264b34bf3dd8a3
eshlykov <e.a.shlykov@gmail.com>
2018-03-01 01:33:01 +0300

    Посчитали ручки

---
+++ CrossingLimits/MaxPensCount.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Windows.h>
+
+// All GDI Objets: https://msdn.microsoft.com/ru-ru/library/windows/desktop/ms724291%28v=vs.85%29.aspx. 
+void PrintMaxPensCount()
+{
+	int pensCount = 0;
+
+	// See: https://msdn.microsoft.com/ru-ru/library/windows/desktop/dd183509(v=vs.85).aspx (CreatePen),
+	// https://msdn.microsoft.com/ru-ru/library/windows/desktop/dd162937(v=vs.85).aspx (RGB).
+	while ( CreatePen( PS_DASH, 1, RGB( 127, 127, 127 ) ) != NULL ) {
+		++pensCount;
+	}
+
+	std::cout << pensCount << " pens were created." << std::endl;
+}
