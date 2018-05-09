f0d42b75dc531759d7820c9985dfae21754b3ac0
eshlykov <e.a.shlykov@gmail.com>
2018-05-09 16:22:26 +0300

    Создал диалоговое окошко

--- Editor/resource.h
+++ Editor/resource.h
@@ -3,15 +3,22 @@
 // Used by Editor.rc
 //
 #define IDR_MENU1                       101
+#define IDD_DIALOG1                     102
+#define IDC_BUTTON3                     1009
+#define IDC_BUTTON4                     1010
+#define IDC_CHECK1                      1011
+#define IDC_SLIDER1                     1013
+#define IDC_SLIDER2                     1014
 #define ID_VIEW_SETTINGS                40001
+#define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        108
 #define _APS_NEXT_COMMAND_VALUE         40002
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
